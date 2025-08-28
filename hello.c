275898dfdd71d4877af5029910d944eed8d24c7a
Ben Smith <bsmith142@unl.edu>
2025-08-28 13:11:29 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Ben Smith
+ *         bsmith142@unl.edu
+ * Date: 2025/28/08
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Ben Smith\n");
+
+  return 0;
+}

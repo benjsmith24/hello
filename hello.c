50e135a3db7aa817d8ca6b0524b5a1bb89921d93
Ben Smith <bsmith142@unl.edu>
2025-08-29 23:01:16 -0500

    added name and major

--- hello.c
+++ hello.c
@@ -9,9 +9,8 @@
     printf("Hello, I'm Keny Moreno!\n");
     printf("Major is Computer Science!\n");
     printf("Minor is non\n");
-
-
-
+    printf("Hello, I'm Ben Smith!\n");
+    printf("Major is Electrical Engineering!\n");
 
     return 0;
  }
